78013fb0af4b5f2b0182a9ad53b69eced75fe64c
SoloStryker <46388630+SoloStryker@users.noreply.github.com>
2021-09-16 10:18:44 -0400

    added convert ps1 file
    this file as is assumes imagemagick is installed

---
+++ convertfiles.ps1
@@ -0,0 +1,13 @@
+﻿if (!(Test-Path Archive))
+{
+    New-Item -Name "Archive" -ItemType "directory"
+}
+
+$fileIn = Get-ChildItem -File -Filter '*.heic'
+foreach ($fileName in $fileIn)
+{
+    #write-host $fileName.basename is a $fileName.extension
+    $fileNameNew = $fileName.BaseName + ".png"
+    magick $fileName.name $fileNameNew
+    Move-Item $fileName Archive
+}
